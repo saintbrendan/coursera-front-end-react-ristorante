20bf8615cc1dc84c943d6e52988dab1c719c9a93
Brendan Cunnie <saintbrendan@gmail.com>
2018-11-22 23:53:59 -0500

    Update from Exercise (Instructions) in course

--- src/components/MenuComponent.js
+++ src/components/MenuComponent.js
@@ -1,9 +1,11 @@
 import React from 'react';
-import { Card, CardImg, CardImgOverlay, CardText, CardBody, CardTitle } from 'reactstrap';
+import { Card, CardImg, CardImgOverlay,
+    CardTitle } from 'reactstrap';
 
-    function RenderMenuItem({ dish, onClick }) {
+    function RenderMenuItem ({dish, onClick}) {
         return (
-            <Card key={dish.id} onClick={() => this.props.onClick(dish.id)}>
+            <Card
+                onClick={() => onClick(dish.id)}>
                 <CardImg width="100%" src={dish.image} alt={dish.name} />
                 <CardImgOverlay>
                     <CardTitle>{dish.name}</CardTitle>
@@ -13,20 +15,21 @@ import { Card, CardImg, CardImgOverlay, CardText, CardBody, CardTitle } from 're
     }
 
     const Menu = (props) => {
-        const menu = this.props.dishes.map((dish) => {
+
+        const menu = props.dishes.map((dish) => {
             return (
-                <div key={dish.id} className="col-12 col-md-5 m-1">
+                <div className="col-12 col-md-5 m-1"  key={dish.id}>
                     <RenderMenuItem dish={dish} onClick={props.onClick} />
                 </div>
             );
         });
 
         return (
-          <div className="container">
-            <div className="row">
-                {menu}
+            <div className="container">
+                <div className="row">
+                    {menu}
+                </div>
             </div>
-          </div>
         );
     }
 
