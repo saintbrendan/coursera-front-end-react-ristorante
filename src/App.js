778c0ea3c19ffac4936dbee7d17446588b34610a
Brendan Cunnie <saintbrendan@gmail.com>
2018-11-24 13:09:44 -0500

    React Router

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
 import React, { Component } from 'react';
-import { Navbar, NavbarBrand } from 'reactstrap';
-import Menu from './components/MenuComponent';
-import './App.css';
-import { DISHES } from './shared/dishes'
 import Main from './components/MainComponent';
+import './App.css';
+import { BrowserRouter } from 'react-router-dom';
 
 class App extends Component {
 
   render() {
     return (
-      <div className="App">
-        <Main />
-      </div>
+      <BrowserRouter>
+        <div className="App">
+          <Main />
+        </div>
+      </BrowserRouter>
     );
   }
 }
