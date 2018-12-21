84df4990c749c9920c86c9a67a8402de2181e48b
Brendan Cunnie <saintbrendan@gmail.com>
2018-12-21 16:02:47 -0500

    Restore CardBody in DishDetailComponent.js

--- src/components/DishDetailComponent.js
+++ src/components/DishDetailComponent.js
@@ -95,8 +95,10 @@ function RenderDish({ dish }) {
     <div key={dish.id} className="col-12 col-md-5 m-1">
       <Card>
         <CardImg width="100%" src={dish.image} alt={dish.name} />
-        <CardTitle>{dish.name}</CardTitle>
-        <CardText>{dish.description}</CardText>
+        <CardBody>
+          <CardTitle>{dish.name}</CardTitle>
+          <CardText>{dish.description}</CardText>
+        </CardBody>
       </Card>
     </div>
   );
