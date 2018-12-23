2346c20686afca9c2b14a4110bd0bb20ecb9e52f
Brendan Cunnie <saintbrendan@gmail.com>
2018-12-23 14:39:46 -0500

    Fetch from Server

--- src/components/DishDetailComponent.js
+++ src/components/DishDetailComponent.js
@@ -8,10 +8,25 @@ import {
 import { Link } from 'react-router-dom';
 import { LocalForm, Control, Errors } from 'react-redux-form';
 import { Loading } from './LoadingComponent';
+import { baseUrl } from '../shared/baseUrl';
 
 const required = (val) => val && val.length;
 const maxLength = (len) => (val) => !(val) || (val.length <= len);
 const minLength = (len) => (val) => val && (val.length >= len);
+
+function RenderDish({ dish }) {
+  return (
+    <div key={dish.id} className="col-12 col-md-5 m-1">
+      <Card>
+        <CardImg top src={baseUrl + dish.image} alt={dish.name} />
+        <CardBody>
+          <CardTitle>{dish.name}</CardTitle>
+          <CardText>{dish.description}</CardText>
+        </CardBody>
+      </Card>
+    </div>
+  );
+}
 
 class CommentForm extends Component {
   constructor(props) {
@@ -88,20 +103,6 @@ class CommentForm extends Component {
       </div>
     );
   }
-}
-
-function RenderDish({ dish }) {
-  return (
-    <div key={dish.id} className="col-12 col-md-5 m-1">
-      <Card>
-        <CardImg width="100%" src={dish.image} alt={dish.name} />
-        <CardBody>
-          <CardTitle>{dish.name}</CardTitle>
-          <CardText>{dish.description}</CardText>
-        </CardBody>
-      </Card>
-    </div>
-  );
 }
 
 function RenderComments({ comments, addComment, dishId }) {
--- src/components/HomeComponent.js
+++ src/components/HomeComponent.js
@@ -4,6 +4,7 @@ import {
     CardTitle, CardSubtitle
 } from 'reactstrap';
 import { Loading } from './LoadingComponent';
+import { baseUrl } from '../shared/baseUrl';
 
 function RenderCard({ item, isLoading, errMess }) {
 
@@ -17,18 +18,23 @@ function RenderCard({ item, isLoading, errMess }) {
             <h4>{errMess}</h4>
         );
     }
-    else
-        return (
-            <Card>
-                <CardImg src={item.image} alt={item.name} />
-                <CardBody>
-                    <CardTitle>{item.name}</CardTitle>
-                    {item.designation ? <CardSubtitle>{item.designation}</CardSubtitle> : null}
-                    <CardText>{item.description}</CardText>
-                </CardBody>
-            </Card>
-        );
-
+    else {
+        if (typeof item === 'undefined') {
+            return (<Card>item is undefined</Card>)
+        } else {
+            return (
+                <Card>
+                    {item.name === 'Alberto Somayya'?<div>item.image:{item.image}</div>:null}
+                    <CardImg src={baseUrl + item.image} alt={item.name} />
+                    <CardBody>
+                        <CardTitle>{item.name}</CardTitle>
+                        {item.designation ? <CardSubtitle>{item.designation}</CardSubtitle> : null}
+                        <CardText>{item.description}</CardText>
+                    </CardBody>
+                </Card>
+            );
+        }
+    }
 }
 
 function Home(props) {
@@ -39,7 +45,7 @@ function Home(props) {
                     <RenderCard item={props.dish} isLoading={props.dishesLoading} errMess={props.dishesErrMess} />
                 </div>
                 <div className="col-12 col-md m-1">
-                    <RenderCard item={props.promotion} />
+                    <RenderCard item={props.promotion} isLoading={props.promoLoading} errMess={props.promoErrMess} />
                 </div>
                 <div className="col-12 col-md m-1">
                     <RenderCard item={props.leader} />
