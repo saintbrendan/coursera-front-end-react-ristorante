90a53b65ad1625f64d17f4627969b7b4508d2396
Brendan Cunnie <saintbrendan@gmail.com>
2018-12-29 14:00:26 -0500

    Assignment 4 Task 2a  Post feedback to server

---
+++ src/redux/feedback.js
@@ -0,0 +1,20 @@
+import * as ActionTypes from './ActionTypes';
+
+export const Feedback = (state = { errMess: null, feedback: null }, action) => {
+    switch (action.type) {
+        case ActionTypes.ADD_FEEDBACK:
+            return { ...state, errMess: null, feedback: action.payload };
+
+        case ActionTypes.FEEDBACK_FAILED:
+            return { ...state, errMess: action.payload };
+
+        ////
+        // case ActionTypes.ADD_FEEDBACK:
+        //     var feedback = action.payload;
+        //     return { ...state, feedback: state.feedback.concat(feedback) };
+        ////
+
+        default:
+            return state;
+    }
+};
